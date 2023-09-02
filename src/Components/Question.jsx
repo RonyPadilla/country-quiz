fbfea86e6e7a8db93b84cd0c5cb1268196696c6f
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-01 22:03:37 -0600

    add styles and componets

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,95 +1,72 @@
-import { useEffect, useState } from "react";
 import "../Style/question.css";
-// import confetti from "canvas-confetti";
+import { Button } from "./Button.jsx";
+import { useState } from "react";
 
 export const Question = () => {
-  const [buttonClass, setButtonClass] = useState([]);
-  const [buttonClassName, setButtonClassName] = useState("");
 
   const questionDefault = [
     {
       id: 1,
       question: "Kuala Lumpur is the capital of",
       option: [
-        ["Vietnam", false],
-        ["Malaysia", true],
-        ["Sweden", false],
-        ["Austria", false],
+        ['A',"Vietnam", false],
+        ['B',"Malaysia", true],
+        ['C',"Sweden", false],
+        ['D',"Austria", false],
       ],
     },
   ];
 
-  useEffect(() => {
-    if (buttonClass[0] == true) {
-      setButtonClassName("-correct");
-    } else {
-      setButtonClassName("-incorrect");
-    }
-  }, [buttonClass]);
+  console.log(questionDefault.map((question) => {
+    return question.option[0][1]
+  }))
 
-  console.log("Hola button inicio " + buttonClass[1]);
-  const verify = (val, id) => {
-    buttonClass.push(val);
-    buttonClass.push(id);
-    setButtonClass(buttonClass);
+
+
+
+  const [valorDesdeHijo, setValorDesdeHijo] = useState(false);
+
+  // Función para recibir el valor del componente hijo
+  const receiveValue = (valor) => {
+    setValorDesdeHijo(valor);
   };
-
-  console.log(questionDefault.map((question) => question.option[0][1]));
-
-  console.log(
-    `button-${
-      buttonClass[0] == true && buttonClass[0] == 1 ? "correct" : "incorrect"
-    }`
-  );
-
-  console.log("Hola button " + buttonClass);
-  console.log("Hola button " + buttonClassName);
-
+  console.log(receiveValue)
   return (
     <>
       <section className="question">
         {questionDefault.map((question) => (
           <div className="question-div" key={question.id}>
             <div className="question-h2">
-              <h2>{question.question}</h2>
+              <h2 >{question.question}</h2>
             </div>
             <div className="question-option-div">
-              <button
-                onClick={() => verify(question.option[0][1], 1)}
-                className={`button${ buttonClassName }`}
-              >
-                <div className="div-option">
-                  <h2>A</h2>
-                </div>
-                <h3>{question.option[0][0]}</h3>
-              </button>
-              <button
-                onClick={() => verify(question.option[1][1])}
-                className={"button"}
-              >
-                <div className="div-option">
-                  <h2>B</h2>
-                </div>
-                <h3>{question.option[1][0]}</h3>
-              </button>
-              <button
-                onClick={() => verify(question.option[2][1])}
-                className={"button"}
-              >
-                <div className="div-option">
-                  <h2>C</h2>
-                </div>
-                <h3>{question.option[2][0]}</h3>
-              </button>
-              <button
-                onClick={() => verify(question.option[3][1])}
-                className={"button"}
-              >
-                <div className="div-option">
-                  <h2>D</h2>
-                </div>
-                <h3>{question.option[3][0]}</h3>
-              </button>
+              <Button 
+              id={1}
+              letter={question.option[0][0]}
+              question={question.option[0][1]}
+              answer={question.option[0][2]}
+              />
+              <Button 
+              id={2}
+              letter={question.option[1][0]}
+              question={question.option[1][1]}
+              answer={question.option[1][2]}
+              
+              />
+              <Button 
+              id={3}
+              letter={question.option[2][0]}
+              question={question.option[2][1]}
+              answer={question.option[2][2]}
+              className={'-correct'}
+              sendValue={receiveValue}
+              />
+              <Button 
+              id={4}
+              letter={question.option[3][0]}
+              question={question.option[3][1]}
+              answer={question.option[3][2]}
+              />
             </div>
           </div>
         ))}
