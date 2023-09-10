8a599c46e0f8e42f61f434e23893ce2945992523
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-10 08:24:36 -0600

    delete totally footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/Style/index.css";
 import { App } from "./Components/App.jsx";
-import Footer from "./Components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Footer />
   </React.StrictMode>
 );
