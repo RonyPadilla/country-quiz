889b63c3745dcbd5ddf63dea494800cf8b862d6b
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-09 21:46:10 -0600

    modified components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,16 @@
 import "../style/footer.css";
 
-export const Footer = () => {
+const Footer = () => {
   return (
     <>
-      <footer className="footer">
-
-          <p className="credits">
-            created by -<a href="https://github.com/RonyPadilla">RonyPadilla</a>
-            - devChallenges.io
-          </p>
-      </footer>
+      <section>
+        <p className="footer">
+          created by - <a href="https://github.com/RonyPadilla">RonyPadilla</a>{" "}
+          - devChallenges.io
+        </p>
+      </section>
     </>
   );
 };
+
+export default Footer 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '../src/Style/index.css'
-import { App } from './Components/App.jsx'
-import { Footer } from './Components/Footer.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/Style/index.css";
+import { App } from "./Components/App.jsx";
+import Footer from "./Components/Footer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Footer />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
