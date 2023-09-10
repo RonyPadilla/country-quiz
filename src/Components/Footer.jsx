1d655502a2c65e283c028453954c2d493e07cc5e
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-09 20:34:38 -0600

    add styles and change components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,11 @@ export const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <div className="div-credits-footer">
+
           <p className="credits">
             created by -<a href="https://github.com/RonyPadilla">RonyPadilla</a>
             - devChallenges.io
           </p>
-        </div>
       </footer>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../src/Style/index.css'
-import '../src/Components/Start.jsx'
-// import { Start } from '../src/Components/Start.jsx'
+import { App } from './Components/App.jsx'
 import { Footer } from './Components/Footer'
-// import { Create } from './Components/Create'
-// import { Question } from '../src/Components/Question'
-//import { Win } from '../src/Components/Win.jsx'
- import { App } from './Components/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Footer/>
+    <Footer />
   </React.StrictMode>,
 )
